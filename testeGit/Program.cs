a80642f0a4812be668c70db59472e9f302aa5b38
Gustavo Correa <tcho32@gmail.com>
2020-08-03 19:29:34 -0300

    teste branch

--- testeGit/Program.cs
+++ testeGit/Program.cs
@@ -7,8 +7,6 @@ namespace testeGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
-            Console.WriteLine("Como você está?");
         }
     }
 }
